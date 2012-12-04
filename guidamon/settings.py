c7d72619febe2ecbeab1ac0184dddefa533efd0f
Diego Phoenix <dpam.cin@gmail.com>
2012-12-04 18:21:17 -0300

    realizando teste com o south

--- guidamon/settings.py
+++ guidamon/settings.py
@@ -124,8 +124,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jogador',
-    'guidu',
+    #'jogador',
+    #'guidu',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
