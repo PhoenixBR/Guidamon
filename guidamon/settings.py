ed85d16f20bec89b2d80445b86d4e0eddb7c573f
Diego Phoenix <dpam.cin@gmail.com>
2013-01-11 09:13:15 -0300

    design ajustavel em telas pequenas

--- guidamon/settings.py
+++ guidamon/settings.py
@@ -15,8 +15,8 @@ MANAGERS = ADMINS
 
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-
-'''DATABASES = {
+'''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'guidamon.db',                      # Or path to database file if using sqlite3.
